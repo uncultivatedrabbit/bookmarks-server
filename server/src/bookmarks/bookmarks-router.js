c83035cd3fad1d7dd1354745cd934214c6af3c18
Brandon Melendez <brandonmelendezdev@gmail.com>
2020-06-08 12:45:34 -0400

    finished PATCH endpoint and functionality

--- server/src/bookmarks/bookmarks-router.js
+++ server/src/bookmarks/bookmarks-router.js
@@ -2,12 +2,11 @@ const express = require("express");
 const bodyParser = express.json();
 const bookmarksRouter = express.Router();
 const logger = require("../logger");
-const { bookmarks } = require("../store");
 const BookmarkServices = require("../bookmark-services");
 const xss = require("xss");
 
 bookmarksRouter
-  .route("/bookmark")
+  .route("/api/bookmark")
   .get((req, res, next) => {
     const knexInstance = req.app.get("db");
     BookmarkServices.getAllBookmarks(knexInstance)
@@ -17,6 +16,7 @@ bookmarksRouter
             id: b.id,
             title: xss(b.title),
             description: xss(b.description),
+            url: xss(b.url),
             rating: +b.rating,
             date_published: b.date_published,
           }))
@@ -25,9 +25,9 @@ bookmarksRouter
       .catch(next);
   })
   .post(bodyParser, (req, res, next) => {
-    const { title, description, rating } = req.body;
+    const { title, description, url, rating } = req.body;
 
-    const newBookmark = { title, description, rating };
+    const newBookmark = { title, description, url, rating };
     // check that all values exist
     for (const [key, value] of Object.entries(newBookmark)) {
       if (value == null) {
@@ -37,21 +37,22 @@ bookmarksRouter
       }
     }
     // check to make sure rating is an int between 1-5
-    if(rating !== +(rating) || rating > 5 || rating < 1){
+    if (+rating !== +rating || rating > 5 || rating < 1) {
       return res.status(400).json({
         error: { message: "rating must be a number between 1 and 5" },
-      })
+      });
     }
-    BookmarkServices.insertBookmark(req.app.get('db'), newBookmark)
+    BookmarkServices.insertBookmark(req.app.get("db"), newBookmark)
       .then((bookmark) => {
         logger.info(`Bookmark created!`);
         res
           .status(201)
-          .location(`/bookmark/${bookmark.id}`)
+          .location(`/api/bookmark/${bookmark.id}`)
           .json({
             id: bookmark.id,
             title: xss(bookmark.title),
             description: xss(bookmark.description),
+            url: xss(bookmark.url),
             rating: +bookmark.rating,
             date_published: bookmark.date_published,
           });
@@ -60,9 +61,9 @@ bookmarksRouter
   });
 
 bookmarksRouter
-  .route("/bookmark/:id")
+  .route("/api/bookmark/:id")
   .all((req, res, next) => {
-    BookmarkServices.getById(req.app.get('db'), req.params.id)
+    BookmarkServices.getById(req.app.get("db"), req.params.id)
       .then((bookmark) => {
         if (!bookmark) {
           return res.status(404).json({
@@ -79,6 +80,7 @@ bookmarksRouter
       id: res.bookmark.id,
       title: xss(res.bookmark.title),
       description: xss(res.bookmark.description),
+      url: xss(res.bookmark.url),
       rating: +res.bookmark.rating,
       date_published: res.bookmark.date_published,
     });
@@ -90,6 +92,28 @@ bookmarksRouter
         res.status(204).end();
       }
     );
+  })
+  .patch(bodyParser, (req, res, next) => {
+    const { title, description, url, rating } = req.body;
+    const bookmarkToUpdate = { title, description, url, rating };
+    const numberOfValues = Object.values(bookmarkToUpdate).filter(Boolean)
+      .length;
+    if (numberOfValues === 0) {
+      return res.status(400).json({
+        error: {
+          message: `Request body must contain either 'title', 'description' or 'rating'`,
+        },
+      });
+    }
+    BookmarkServices.updateBookmark(
+      req.app.get("db"),
+      req.params.id,
+      bookmarkToUpdate
+    )
+      .then((numRowsAffected) => {
+        res.status(204).end();
+      })
+      .catch(next);
   });
 
 module.exports = bookmarksRouter;
--- server/test/bookmark-endpoints.spec.js
+++ server/test/bookmark-endpoints.spec.js
@@ -19,7 +19,7 @@ describe.only("Bookmark endpoint", function () {
 
   afterEach("cleanup", () => db("bookmarks").truncate());
   // GET REQUESTS
-  describe("GET /bookmark", () => {
+  describe("GET /api/bookmark", () => {
     // WHEN THERE ARE BOOKMARKS IN DB
     context("Given there are bookmarks in the database", () => {
       const testBookmarks = makeBookmarksArray();
@@ -28,19 +28,19 @@ describe.only("Bookmark endpoint", function () {
         return db.into("bookmarks").insert(testBookmarks);
       });
 
-      it("GET /bookmark responds with 200 and all of the bookmarks", () => {
-        return supertest(app).get("/bookmark").expect(200, testBookmarks);
+      it("GET /api/bookmark responds with 200 and all of the bookmarks", () => {
+        return supertest(app).get("/api/bookmark").expect(200, testBookmarks);
       });
     });
     //WHEN THERE ARE NO BOOKMARKS IN DB
     context("given no bookmarks", () => {
       it("responds with 200 and empty array", () => {
-        return supertest(app).get("/bookmark").expect(200, []);
+        return supertest(app).get("/api/bookmark").expect(200, []);
       });
     });
   });
 
-  describe("GET /bookmark/:id", () => {
+  describe("GET /api/bookmark/:id", () => {
     context("Given there are bookmarks in the database", () => {
       const testBookmarks = makeBookmarksArray();
 
@@ -52,7 +52,7 @@ describe.only("Bookmark endpoint", function () {
         const bookmarkId = 2;
         const expectedBookmark = testBookmarks[bookmarkId - 1];
         return supertest(app)
-          .get(`/bookmark/${bookmarkId}`)
+          .get(`/api/bookmark/${bookmarkId}`)
           .expect(200, expectedBookmark);
       });
     });
@@ -63,6 +63,7 @@ describe.only("Bookmark endpoint", function () {
         id: 911,
         title: 'Naughty naughty very naughty <script>alert("xss");</script>',
         description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+        url: "https://google.com",
         rating: 0,
       };
       beforeEach("insert malicious article", () => {
@@ -70,7 +71,7 @@ describe.only("Bookmark endpoint", function () {
       });
       it("removes XSS attack content", () => {
         return supertest(app)
-          .get(`/bookmark`)
+          .get(`/api/bookmark`)
           .expect(200)
           .expect((res) => {
             expect(res.body[0].title).to.eql(
@@ -87,13 +88,13 @@ describe.only("Bookmark endpoint", function () {
       it("responds with a 404", () => {
         const bookmarkId = 6854;
         return supertest(app)
-          .get(`/bookmark/${bookmarkId}`)
+          .get(`/api/bookmark/${bookmarkId}`)
           .expect(404, { error: { message: "Bookmark doesn't exist." } });
       });
     });
   });
   // POST REQUESTS
-  describe("POST /bookmark", () => {
+  describe("POST /api/bookmark", () => {
     it("creates a bookmark, responding with 201 and the new bookmark", function () {
       const newBookmark = {
         title: "Test new bookmark",
@@ -102,19 +103,20 @@ describe.only("Bookmark endpoint", function () {
       };
 
       return supertest(app)
-        .post("/bookmark")
+        .post("/api/bookmark")
         .send(newBookmark)
         .expect(201)
         .expect((res) => {
           expect(res.body.title).to.eql(newBookmark.title);
           expect(res.body.description).to.eql(newBookmark.description);
           expect(res.body.rating).to.eql(newBookmark.rating);
+          expect(res.body.url).to.eql(newBookmark.url)
           expect(res.body).to.have.property("id");
-          expect(res.headers.location).to.eql(`/bookmark/${res.body.id}`);
+          expect(res.headers.location).to.eql(`/api/bookmark/${res.body.id}`);
         })
         .then((postRes) =>
           supertest(app)
-            .get(`/bookmark/${postRes.body.id}`)
+            .get(`/api/bookmark/${postRes.body.id}`)
             .expect(postRes.body)
         );
     });
@@ -130,7 +132,7 @@ describe.only("Bookmark endpoint", function () {
       it(`responds with 400 and an error message when the ${field} is missing`, () => {
         delete newBookmark[field];
         return supertest(app)
-          .post("/bookmark")
+          .post("/api/bookmark")
           .send(newBookmark)
           .expect(400, {
             error: { message: `Missing ${field} in request body` },
@@ -139,13 +141,13 @@ describe.only("Bookmark endpoint", function () {
     });
   });
 
-  // DELTE tests
-  describe("DELETE /bookmark/:id", () => {
+  // DELETE tests
+  describe("DELETE /api/bookmark/:id", () => {
     context("Given there are NO bookmarks", () => {
       it("responds with a 404", () => {
         const bookmarkId = 123456;
         return supertest(app)
-          .delete(`/bookmark/${bookmarkId}`)
+          .delete(`/api/bookmark/${bookmarkId}`)
           .expect(404, { error: { message: "Bookmark doesn't exist." } });
       });
     });
@@ -162,11 +164,87 @@ describe.only("Bookmark endpoint", function () {
           (bookmark) => bookmark.id !== idToRemove
         );
         return supertest(app)
-          .delete(`/bookmark/${idToRemove}`)
+          .delete(`/api/bookmark/${idToRemove}`)
           .expect(204)
           .then((res) =>
-            supertest(app).get("/bookmark").expect(expectedBookmarks)
+            supertest(app).get("/api/bookmark").expect(expectedBookmarks)
           );
+      });
+    });
+  });
+
+  describe("PATCH /api/bookmark/:id", () => {
+    context("Given no bookmarks", () => {
+      it("responds with 404", () => {
+        const bookmarkId = 123456;
+        return supertest(app)
+          .patch(`/api/bookmark/${bookmarkId}`)
+          .expect(404, { error: { message: "Bookmark doesn't exist." } });
+      });
+    });
+
+    context("Given bookmarks in the database", () => {
+      const testBookmarks = makeBookmarksArray();
+      beforeEach("insert bookmarks", () => {
+        return db.into("bookmarks").insert(testBookmarks);
+      });
+
+      it("responds with 204 and updates the bookmark", () => {
+        const idToUpdate = 2;
+        const updateBookmark = {
+          title: "updated article title",
+          description: "updated description",
+          rating: 5,
+          url: "https://google.com",
+        };
+        const expectedBookmark = {
+          ...testBookmarks[idToUpdate - 1],
+          ...updateBookmark,
+        };
+        return supertest(app)
+          .patch(`/api/bookmark/${idToUpdate}`)
+          .send(updateBookmark)
+          .expect(204)
+          .then((res) =>
+            supertest(app)
+              .get(`/api/bookmark/${idToUpdate}`)
+              .expect(expectedBookmark)
+          );
+      });
+
+      it(`responds with 400 when no required fields supplied`, () => {
+        const idToUpdate = 2;
+        return supertest(app)
+          .patch(`/api/bookmark/${idToUpdate}`)
+          .send({ irrelevantField: "bar" })
+          .expect(400, {
+            error: {
+              message: `Request body must contain either 'title', 'description' or 'rating'`,
+            },
+          });
+      });
+
+      it("responds with a 204 when updating only a subset of fields", () => {
+        const idToUpdate = 3;
+        const updateBookmark = {
+          title: "updated bookmark title",
+        };
+        const expectedBookmark = {
+          ...testBookmarks[idToUpdate - 1],
+          ...updateBookmark,
+        };
+        return supertest(app)
+          .patch(`/api/bookmark/${idToUpdate}`)
+          .send({
+            ...updateBookmark,
+            fieldToIgnore: "should not be in GET response",
+          })
+          .expect(204)
+          .then((res) => {
+            return supertest(app)
+              .get(`/api/bookmark/${idToUpdate}`)
+              .expect(expectedBookmark);
+          });
       });
     });
   });
